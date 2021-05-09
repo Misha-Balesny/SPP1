a1e669aa5c91dffab42fec3259d2d5da817b90af
Babulesny <misha271000@gmail.com>
2021-05-09 23:18:07 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const fileUpload = require('express-fileupload') 
+const appRoutes = require('./routes/routes')
+const multer = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+
+const storageConfig = multer.diskStorage({
+    destination:(req,file, cb)=>{
+        cb(null, "uploads")
+    },
+    filename:(req,file,cb)=>{
+        cb(null,file.originalname)
+    }
+})
+
+app.engine('.hbs', exphbs({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views','views')
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
+app.use(multer({storage:storageConfig}).single("filedata"))
+app.use(appRoutes)
+app.use(fileUpload({}))
+
+async function start(){
+    try{
+        await mongoose.connect("mongodb+srv://misha:12345@cluster0.gcqt1.mongodb.net/app?retryWrites=true&w=majority",
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology:true,
+                useFindAndModify:false            
+            }
+        )
+        app.listen(PORT,() => {
+            console.log('Server has been started...')
+        })
+    }catch(e){
+     console.log(e)
+    }
+}
+
+start()
